23c6fe131d7e27cbfccda7d92f75a6dcec67eb1f
Gustavo Leal <gustavo.leal@cronosbank.com.br>
2024-06-26 16:52:24 -0300

    (Separating Responsibilities)

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
         backgroundColor: Colors.blueAccent,
         body: Center(
